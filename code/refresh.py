bbf612314b3cca6542ba447b6e72526b3b0b285f
Kevin Baragona <kmbaragona@gmail.com>
2018-10-05 20:56:36 -0700

    add try except in the two loops so an error does not block other operation...

--- code/refresh.py
+++ code/refresh.py
@@ -1,10 +1,12 @@
-import requests
-import boto3
-import os, time, sys, subprocess
 import os.path
 import signal
+import subprocess
+import traceback
 from os import listdir
 from os.path import isfile, join
+
+import boto3
+import requests
 
 r = requests.get("http://169.254.169.254/latest/dynamic/instance-identity/document")
 response_json = r.json()
@@ -21,38 +23,47 @@ print tags
 
 ports_to_keep = set()
 for tag in tags:
-    key = tag['Key']
-    value = tag['Value']
-    if not key.isdigit():
-        print('key is not a port number, skipping:' + key)
-        continue
-    ports_to_keep.add(key)
-    targetfile = 'listeners/%s' % (key)
-    pidfile = 'pidfiles/%s' % (key)
-    if os.path.isfile(targetfile) and open(targetfile, 'rb').read() == value:
-        print('listener already exists for ' + key)
-        continue
-    if os.path.isfile(targetfile) and open(targetfile, 'rb').read() != value:
-        print('listener already exists but has the wrong target ' + key)
-        tokill = int(open(pidfile, 'rb').read())
-        os.killpg(os.getpgid(tokill), signal.SIGKILL)
-    print('creating new listener...')
-    process = subprocess.Popen(['python', 'keep_alive.py', 'socat', 'TCP-LISTEN:' + key + ',fork', 'TCP:' + value],
-                               close_fds=True, preexec_fn=os.setsid)
-    pid = process.pid
-    print('created socat pid:' + str(pid))
-    open(targetfile, 'wb').write(value)
-    open(pidfile, 'wb').write(str(pid))
+    try:
+        key = tag['Key']
+        ports_to_keep.add(key) # do this early in the try catch
+        value = tag['Value']
+        if not key.isdigit():
+            print('key is not a port number, skipping:' + key)
+            continue
+        targetfile = 'listeners/%s' % (key)
+        pidfile = 'pidfiles/%s' % (key)
+        if os.path.isfile(targetfile) and open(targetfile, 'rb').read() == value:
+            print('listener already exists for ' + key)
+            continue
+        if os.path.isfile(targetfile) and open(targetfile, 'rb').read() != value:
+            print('listener already exists but has the wrong target ' + key)
+            tokill = int(open(pidfile, 'rb').read())
+            os.killpg(os.getpgid(tokill), signal.SIGKILL)
+        print('creating new listener...')
+        process = subprocess.Popen(['python', 'keep_alive.py', 'socat', 'TCP-LISTEN:' + key + ',fork', 'TCP:' + value],
+                                   close_fds=True, preexec_fn=os.setsid)
+        pid = process.pid
+        print('created socat pid:' + str(pid))
+        open(targetfile, 'wb').write(value)
+        open(pidfile, 'wb').write(str(pid))
+    except Exception: # I know what you're thinking and you're wrong
+        print('Caught error looping over instance tags:')
+        traceback.print_exc()
+
 
 # now kill any listeners that should go away b/c the tag went byebye
 existing_listener_ports = [f for f in listdir('listeners/') if isfile(join('listeners/', f))]
 for existing_port in existing_listener_ports:
     if existing_port not in ports_to_keep:
-        print('listener should go bye-bye:' + existing_port)
-        targetfile = 'listeners/%s' % (existing_port)
-        pidfile = 'pidfiles/%s' % (existing_port)
-        tokill = int(open(pidfile, 'rb').read())
-        os.killpg(os.getpgid(tokill), signal.SIGKILL)
-        os.remove(pidfile)
-        os.remove(targetfile)
-        print('fully removed listener:' + existing_port)
+        try:
+            print('listener should go bye-bye:' + existing_port)
+            targetfile = 'listeners/%s' % (existing_port)
+            pidfile = 'pidfiles/%s' % (existing_port)
+            tokill = int(open(pidfile, 'rb').read())
+            os.killpg(os.getpgid(tokill), signal.SIGKILL)
+            os.remove(pidfile)
+            os.remove(targetfile)
+            print('fully removed listener:' + existing_port)
+        except Exception: # I know what you're thinking and you're wrong
+            print('Caught error looping over existing listner ports:')
+            traceback.print_exc()
